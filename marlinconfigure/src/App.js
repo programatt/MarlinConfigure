a59664f2becd8ce2f7b05d953ab7974b9e78726b
Matt Phillips <mathewrphillips@gmail.com>
2018-10-18 14:01:59 -0500

    temp

--- marlinconfigure/src/App.js
+++ marlinconfigure/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-           Herp Derp Derp and Stuff  
+           Herp Derp Derp and Stuff
           </p>
           <a
             className="App-link"
